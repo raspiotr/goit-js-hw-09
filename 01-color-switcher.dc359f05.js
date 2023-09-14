034d2c148f2ca2130d5bd35acbe0bc525bf733e0
raspiotr <130771065+raspiotr@users.noreply.github.com>
2023-09-14 13:47:34 +0000

    Deploying to gh-pages from @ raspiotr/goit-js-hw-09@ea1a5a06312c2e2645e4229589e967f2b36f3f09 🚀

---
+++ 01-color-switcher.dc359f05.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");r.setAttribute("disabled","true");let d=null;e.addEventListener("click",(()=>{e.setAttribute("disabled","true"),r.removeAttribute("disabled"),d=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e}),1e3)})),r.addEventListener("click",(()=>{clearInterval(d),e.removeAttribute("disabled"),r.setAttribute("disabled","true")}));
+//# sourceMappingURL=01-color-switcher.dc359f05.js.map
