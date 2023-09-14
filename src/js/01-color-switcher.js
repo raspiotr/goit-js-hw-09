ea1a5a06312c2e2645e4229589e967f2b36f3f09
raspiotr <raspiotr@gmail.com>
2023-09-14 15:47:10 +0200

    task 01 ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+stopBtn.setAttribute('disabled', 'true');
+let timerId = null;
+
+const colorSwitcher = () => {
+  startBtn.setAttribute('disabled', 'true');
+  stopBtn.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    const newColor = getRandomHexColor();
+    body.style.backgroundColor = newColor;
+  }, 1000);
+};
+
+const colorStop = () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'true');
+};
+
+startBtn.addEventListener('click', colorSwitcher);
+stopBtn.addEventListener('click', colorStop);
